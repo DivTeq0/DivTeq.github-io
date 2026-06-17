44ae3cba70506e215a375dd0aa0d6e6bdf03b3f0
DivTeq0 <frooney@math.wisc.edu>
2026-06-17 18:08:27 +0000

    Update to PreTeXt project source.

--- output/stage/lunr-pretext-search-index.js
+++ output/stage/lunr-pretext-search-index.js
@@ -1198,6 +1198,15 @@ var ptx_lunr_docs = [
   "body": " Find at the point on the surfacw   "
 },
 {
+  "id": "my-great-book-12-2",
+  "level": "1",
+  "url": "my-great-book-12-2.html",
+  "type": "Section",
+  "number": "10.1",
+  "title": "The Gradient",
+  "body": " The Gradient  The definition of differentiability in single variable calculus is the there is a function given by This can be rewritten in the form A function of more than one variable is differentiable if there exists a vector, , called the gradient of , so that where is a unit vector.  "
+},
+{
   "id": "backmatter-2",
   "level": "1",
   "url": "backmatter-2.html",
