6b45b8d9106155b46ac5f9985a60f72be496836b
DivTeq0 <frooney@math.wisc.edu>
2026-06-18 18:06:03 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -1204,7 +1204,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "10.1",
   "title": "The Gradient",
-  "body": " The Gradient  The definition of differentiability in single variable calculus is the there is a function given by To generalize this to functions of more than one variable, this can be rewritten in the form A function of more than one variable is differentiable if there exists a vector, , called the gradient of , so that where is a unit vector. If it is differentiable the previous equation can be rewritten as The expression is called the directional derivative of in the direction   To find the components of . First take . Then the component of is   "
+  "body": " The Gradient  The definition of differentiability in single variable calculus is the there is a function given by To generalize this to functions of more than one variable, this can be rewritten in the form A function of more than one variable is differentiable if there exists a vector, , called the gradient of , so that where is a unit vector. If it is differentiable the previous equation can be rewritten as The expression is called the directional derivative of in the direction   To find the components of . First take . Then the component of is Similarly the and components are and   "
 },
 {
   "id": "backmatter-2",
